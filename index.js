109a28aa90df2b3ac2772f859b8aae64a04c1a2d
Dai Cao <dai.trong.cao@aioz.io>
2019-03-27 17:09:24 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const { app, BrowserWindow } = require('electron')
+const log = require('electron-log')
+const { autoUpdater } = require('electron-updater')
+
+log.transports.file.level = "debug";
+autoUpdater.logger = log;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 app.on('ready', () => {
     let win = new BrowserWindow({ width: 800, height: 800 })
-    win.loadFile('./mainapp/dist/mainapp/index.html')
+    win.loadFile('index.html')
     win.webContents.openDevTools()
+    autoUpdater.checkForUpdatesAndNotify();
 })
